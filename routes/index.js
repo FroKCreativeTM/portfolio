a50c3474a487a2ecbd4b5837fc475b3cc5bd8a2f
seungCheolCha <cscculture95@gmail.com>
2021-10-03 11:33:19 +0900

    main.html 수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('main', { title : 'FroK 포트폴리오' });
+    res.render('main', { title : `Cha Seung Cheol's Portfolio` });
 });
 
 module.exports = router;
