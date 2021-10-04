59e995baa8c9499b4875f63609125a8b58bac962
seungCheolCha <cscculture95@gmail.com>
2021-10-04 18:16:35 +0900

    header 부분 일부 완성

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const fs = require('fs');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('main', { title : `Cha Seung Cheol's Portfolio` });
+});
+
+router.get('/javascript', (req, res) => {
+    fs.readFile('./src/javascript.png', (err, data) => {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(data);
+    });
+});
+
+router.get('/nodejs', (req, res) => {
+    fs.readFile('./src/nodejs.png', (err, data) => {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(data);
+    });
 });
 
 module.exports = router;
